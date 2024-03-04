fd1c0430daa0f3477e2b78750766a30780d1880a
Yigrashill <tomek198734@wp.pl>
2024-03-04 21:49:23 +0100

    Initial Approach to Finance Domain Model

--- src/BackEnd/Domain/Models/Finance.cs
+++ src/BackEnd/Domain/Models/Finance.cs
@@ -5,4 +5,17 @@ public class Finance
     public int Id { get; set; }
     public string Name { get; set; }
     public FinanceType Type { get; set; }
+    public decimal Amount { get; set; } = 0.00M;
+    public DateTime DateTime { get; init; }
+
+    public Finance()
+    {
+        DateTime = DateTime.Now;
+    }
+
+    public Finance(DateTime date)
+    {
+        DateTime = date;
+    }
+
 }
---
+++ src/BackEnd/Domain/Models/MonthlyStatement.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain.Models;
+
+public class MonthlyStatement
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public List<Finance> MonthBalance { get; set; }
+    public DateOnly Month { get; set; }
+}
