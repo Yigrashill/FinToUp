ea6a7edecddc9b8c242d5329d67de6613f980696
Yigrashill <tomek198734@wp.pl>
2024-03-10 10:03:04 +0100

    Add UpdateFinanceCommandHandler to update finance

--- src/BackEnd/Application/Features/Finance/Command/CreateFinance/CreateFinanceCommand.cs
+++ src/BackEnd/Application/Features/Finance/Command/CreateFinance/CreateFinanceCommand.cs
@@ -1,39 +1,11 @@
-using Application.Contracts.Persistance;
-using AutoMapper;
 using Domain.Models;
 using MediatR;
 
 namespace Application.Features.Finance.Command.CreateFinance;
-
 
 public class CreateFinanceCommand : IRequest<int>
 {
     public string Title { get; set; }
     public FinanceType FinanceType { get; set; }
     public decimal Amount { get; set; } = 0.00M;
-}
-
-public class CreateFinanceCommandHandler : IRequestHandler<CreateFinanceCommand, int>
-{
-    private readonly IMapper _mapper;
-    private readonly IFinanceRepository _financeRepository;
-
-    public CreateFinanceCommandHandler(IMapper mapper, IFinanceRepository financeRepository)
-    {
-        _mapper = mapper;
-        _financeRepository = financeRepository;
-    }
-
-    public async Task<int> Handle(CreateFinanceCommand request, CancellationToken cancellationToken)
-    {
-        // TODO Validate data
-
-        // Convert object to entity domain
-        var finance = _mapper.Map<Domain.Models.Finance>(request);
-
-        // Save in database
-        await _financeRepository.CreateAsync(finance); 
-
-        return finance.Id;
-    }
 }
---
+++ src/BackEnd/Application/Features/Finance/Command/CreateFinance/CreateFinanceCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using MediatR;
+using Application.Contracts.Persistance;
+
+namespace Application.Features.Finance.Command.CreateFinance;
+
+public class CreateFinanceCommandHandler : IRequestHandler<CreateFinanceCommand, int>
+{
+    private readonly IMapper _mapper;
+    private readonly IFinanceRepository _financeRepository;
+
+    public CreateFinanceCommandHandler(IMapper mapper, IFinanceRepository financeRepository)
+    {
+        _mapper = mapper;
+        _financeRepository = financeRepository;
+    }
+
+    public async Task<int> Handle(CreateFinanceCommand request, CancellationToken cancellationToken)
+    {
+        // TODO Validate data
+
+        // Convert object to entity domain
+        var finance = _mapper.Map<Domain.Models.Finance>(request);
+
+        // Save in database
+        await _financeRepository.CreateAsync(finance); 
+
+        return finance.Id;
+    }
+}
