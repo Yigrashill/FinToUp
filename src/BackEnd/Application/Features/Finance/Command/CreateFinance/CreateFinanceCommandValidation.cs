47df15dbc20e3e965406b61b34ff93cdf093a24d
Yigrashill <tomek198734@wp.pl>
2024-03-11 06:32:56 +0100

    Add Simple Validation for CreateFinanceCommand.

---
+++ src/BackEnd/Application/Features/Finance/Command/CreateFinance/CreateFinanceCommandValidation.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace Application.Features.Finance.Command.CreateFinance;
+
+public class CreateFinanceCommandValidation : AbstractValidator<CreateFinanceCommand>
+{
+    public CreateFinanceCommandValidation()
+    {
+        RuleFor(f => f.Title)
+            .NotEmpty().NotNull().WithMessage("{PropertyName} is requried ")
+            .MaximumLength(70).WithMessage("{PropertyNme} must be fewer than 70 characters");
+    }
+}
+
