b2dc41953dda80b19ed760dadaf8777779b93a30
Yigrashill <tomek198734@wp.pl>
2024-03-12 05:50:15 +0100

    Create new project Liberary Persistance

---
+++ src/BackEnd/Persistance/PersistanceServiceRegestration.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Persistance;
+
+public static class PersistanceServiceRegestration 
+{
+    public static IServiceCollection AddPersistanceServices(this IServiceCollection services)
+    {
+        return services;
+    }
+}
