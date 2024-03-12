fa7cdbee2da06cbc372580df7f38043f3ded9f8e
Yigrashill <tomek198734@wp.pl>
2024-03-12 06:18:54 +0100

    SaveChanges method

--- src/BackEnd/Persistance/DatabaseContext/FinanceDataBasecontext.cs
+++ src/BackEnd/Persistance/DatabaseContext/FinanceDataBasecontext.cs
@@ -1,4 +1,5 @@
 ﻿using Domain.Models;
+using Domain.Models.Common;
 using Microsoft.EntityFrameworkCore;
 
 public class FinanceDataBaseContext :DbContext
@@ -13,5 +14,20 @@ public class FinanceDataBaseContext :DbContext
     public DbSet<MonthlyStatement> MonthlyStatements { get; set; }
 
 
+    public override Task<int> SaveChangesAsync( CancellationToken cancellationToken = default)
+    {
+        foreach (var entity in base.ChangeTracker.Entries<BaseEntity>()
+            .Where(q => q.State == EntityState.Modified || q.State == EntityState.Added)
+            ) 
+        {
+            entity.Entity.Updated = DateTime.Now;
 
+            if (entity.State == EntityState.Added)
+            {
+                entity.Entity.Createrd = DateTime.Now;
+            }
+        }
+
+        return base.SaveChangesAsync(cancellationToken);
+    }
 }
