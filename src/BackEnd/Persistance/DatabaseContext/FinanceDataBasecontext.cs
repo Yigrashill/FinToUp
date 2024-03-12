ad1119a132cc501431104ee832a40900679d3ca5
Yigrashill <tomek198734@wp.pl>
2024-03-12 06:03:16 +0100

    Add DbContext Class

---
+++ src/BackEnd/Persistance/DatabaseContext/FinanceDataBasecontext.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+public class FinanceDataBaseContext :DbContext
+{
+    public FinanceDataBaseContext(DbContextOptions<FinanceDataBaseContext> options)
+        :base(options)
+    {
+    }
+
+    public DbSet<Finance>  Finances { get; set; }
+
+    public DbSet<MonthlyStatement> MonthlyStatements { get; set; }
+
+
+
+}
