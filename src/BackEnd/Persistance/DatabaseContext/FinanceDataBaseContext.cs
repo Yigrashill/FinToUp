231ffb2179b8f56e6d0ff1d79f4f797ab7755b54
Yigrashill <tomek198734@wp.pl>
2024-03-12 06:18:30 +0100

    Ovverider SaveChangest Metchod, for update and save DateTime.

---
+++ src/BackEnd/Persistance/DatabaseContext/FinanceDataBaseContext.cs
@@ -0,0 +1,33 @@
+﻿using Domain.Models;
+using Domain.Models.Common;
+using Microsoft.EntityFrameworkCore;
+
+public class FinanceDataBaseContext :DbContext
+{
+    public FinanceDataBaseContext(DbContextOptions<FinanceDataBaseContext> options)
+        :base(options)
+    {
+    }
+
+    public DbSet<Finance>  Finances { get; set; }
+
+    public DbSet<MonthlyStatement> MonthlyStatements { get; set; }
+
+
+    public override Task<int> SaveChangesAsync( CancellationToken cancellationToken = default)
+    {
+        foreach (var entity in base.ChangeTracker.Entries<BaseEntity>()
+            .Where(q => q.State == EntityState.Modified || q.State == EntityState.Added)
+            ) 
+        {
+            entity.Entity.Updated = DateTime.Now;
+
+            if (entity.State == EntityState.Added)
+            {
+                entity.Entity.Createrd = DateTime.Now;
+            }
+        }
+
+        return base.SaveChangesAsync(cancellationToken);
+    }
+}
