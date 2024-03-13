853d962a026eda3dd284f94a9ae4af1993244759
Yigrashill <tomek198734@wp.pl>
2024-03-13 15:03:31 +0100

    Create new builder OnModelCreating to add new items when   DataBase is createrd.

--- src/BackEnd/Persistance/DatabaseContext/FinanceDataBasecontext.cs
+++ src/BackEnd/Persistance/DatabaseContext/FinanceDataBasecontext.cs
@@ -2,23 +2,54 @@
 using Domain.Models.Common;
 using Microsoft.EntityFrameworkCore;
 
-public class FinanceDataBaseContext :DbContext
+public class FinanceDataBaseContext : DbContext
 {
     public FinanceDataBaseContext(DbContextOptions<FinanceDataBaseContext> options)
-        :base(options)
+        : base(options)
     {
     }
 
-    public DbSet<Finance>  Finances { get; set; }
+    public DbSet<Finance> Finances { get; set; }
 
     public DbSet<MonthlyStatement> MonthlyStatements { get; set; }
 
 
-    public override Task<int> SaveChangesAsync( CancellationToken cancellationToken = default)
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(FinanceDataBaseContext).Assembly);
+
+        // TODO add MonthStatement and then add Finance records
+        // For simple use and start working whit data base we focus ony simple Finance records 
+        // with no relations bettwen tables.
+
+        modelBuilder.Entity<Finance>().HasData(
+            new Finance
+            {
+                Id = 1,
+                Title = "Biedronka",
+                Createrd = DateTime.Now,
+                FinanceType = FinanceType.Liabilities,
+                Amount = -100.00M
+            },
+            new Finance
+            {
+                Id = 2,
+                Title = "Wypłata",
+                Createrd = DateTime.Now,
+                FinanceType = FinanceType.Liabilities,
+                Amount = 1000.00M
+            }
+            );
+
+        base.OnModelCreating(modelBuilder);
+    }
+
+
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
     {
         foreach (var entity in base.ChangeTracker.Entries<BaseEntity>()
             .Where(q => q.State == EntityState.Modified || q.State == EntityState.Added)
-            ) 
+            )
         {
             entity.Entity.Updated = DateTime.Now;
 
