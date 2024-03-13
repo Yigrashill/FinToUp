adf1a03ff4016ea34eb51c449d68ef187860c8c6
Yigrashill <tomek198734@wp.pl>
2024-03-13 15:10:00 +0100

    Add DbContext regestration

--- src/BackEnd/Persistance/PersistanceServiceRegestration.cs
+++ src/BackEnd/Persistance/PersistanceServiceRegestration.cs
@@ -1,11 +1,19 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Persistance;
 
 public static class PersistanceServiceRegestration 
 {
-    public static IServiceCollection AddPersistanceServices(this IServiceCollection services)
+    public static IServiceCollection AddPersistanceServices(this IServiceCollection services,
+        IConfiguration configuration)
     {
+        services.AddDbContext<FinanceDataBaseContext>(options =>
+        {
+            options.UseSqlServer(configuration.GetConnectionString("FinanceConnectionString"));
+        });
+
         return services;
     }
 }
