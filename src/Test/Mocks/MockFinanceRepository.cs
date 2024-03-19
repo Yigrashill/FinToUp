f90d4c27f7c5e8bcc563d6aa229036a91616155f
Yigrashill <tomek198734@wp.pl>
2024-03-19 07:25:20 +0100

    Create MockIFinancereporitory

---
+++ src/Test/Mocks/MockFinanceRepository.cs
@@ -0,0 +1,32 @@
+﻿using Application.Contracts.Persistance;
+using Domain.Models;
+using Moq;
+
+namespace Test.Mocks;
+
+public class MockFinanceRepository
+{
+    public static Mock<IFinanceRepository> GetMockFinanceRepository()
+    {
+        var finances = new List<Finance>
+        {
+            new Finance() {Id = 1, Title = "Biedronka", Amount = -50.00M},
+            new Finance() {Id = 2, Title = "Wypłata", Amount = 1000.00M},
+        };
+
+        var mockRepo = new Mock<IFinanceRepository>();
+
+        // Mock GetAllFinance
+        mockRepo.Setup(m => m.GetAsync()).ReturnsAsync(finances);
+
+        //Mock Create New Finance
+        mockRepo.Setup(m => m.CreateAsync(It.IsAny<Finance>()))
+            .Returns((Finance finance) =>
+            {
+                finances.Add(finance);
+                return Task.CompletedTask;
+            });
+
+        return mockRepo;
+    }
+}
