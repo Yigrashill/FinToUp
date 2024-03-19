e8f9a1f77c1958b9a7f486f547c2530cde8d8342
Yigrashill <tomek198734@wp.pl>
2024-03-19 09:39:05 +0100

    Add 10 new Finacne Records

--- src/Test/Mocks/MockFinanceRepository.cs
+++ src/Test/Mocks/MockFinanceRepository.cs
@@ -10,8 +10,16 @@ public class MockFinanceRepository
     {
         var finances = new List<Finance>
         {
-            new Finance() {Id = 1, Title = "Biedronka", Amount = -50.00M},
-            new Finance() {Id = 2, Title = "Wypłata", Amount = 1000.00M},
+            new Finance() {Id = 1, Title = "Biedronka", Amount = 50.00M , FinanceType= FinanceType.Liabilities},
+            new Finance() {Id = 2, Title = "Wypłata", Amount = 1000.00M, FinanceType = FinanceType.Assets},
+            new Finance() {Id = 3, Title = "Sklep ABC", Amount = 150.00M, FinanceType = FinanceType.Liabilities},
+            new Finance() {Id = 4, Title = "Czynsz", Amount = 600.00M, FinanceType = FinanceType.Liabilities},
+            new Finance() {Id = 5, Title = "Abonament telefoniczny", Amount = 50.00M, FinanceType = FinanceType.Liabilities},
+            new Finance() {Id = 6, Title = "Zwrot podatku", Amount = 500.00M, FinanceType = FinanceType.Assets},
+            new Finance() {Id = 7, Title = "Rachunek za prąd", Amount = 100.00M, FinanceType = FinanceType.Liabilities},
+            new Finance() {Id = 8, Title = "Oszczędności", Amount = 3000.00M, FinanceType = FinanceType.Assets},
+            new Finance() {Id = 9, Title = "Koszty samochodu", Amount = 200.00M, FinanceType = FinanceType.Liabilities},
+            new Finance() {Id = 10, Title = "Inwestycje", Amount = 1500.00M, FinanceType = FinanceType.Assets},
         };
 
         var mockRepo = new Mock<IFinanceRepository>();
