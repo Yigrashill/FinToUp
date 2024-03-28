9148db875995fc4c925342f40fe2f5088bfdb7c6
Yigrashill <tomek198734@wp.pl>
2024-03-28 06:43:29 +0100

    Update Finance Rest Api method and Uint test

--- src/Test/API.Controllers/FinanceControllerTest.cs
+++ src/Test/API.Controllers/FinanceControllerTest.cs
@@ -1,4 +1,6 @@
-﻿using Application.Features.Finance.Queries;
+﻿using Application.Features.Finance.Command.UpdateCommand;
+using Application.Features.Finance.Queries;
+using Domain.Models;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.EntityFrameworkCore;
@@ -55,7 +57,7 @@ public class FinanceControllerTest : IClassFixture<WebApplicationFactory<Program
         var client = _factory.CreateClient();
 
         // Act
-        var response = await client.GetAsync(ApiUrl.Finances(1));
+        var response = await client.GetAsync("/api/finances/1");
         var result = await response.Content.ReadFromJsonAsync<FinanceDTO>();
 
         // Assert
@@ -70,12 +72,33 @@ public class FinanceControllerTest : IClassFixture<WebApplicationFactory<Program
         var client = _factory.CreateClient();
 
         // Act
-        var response = await client.GetAsync(ApiUrl.Finances());
+        var response = await client.GetAsync("/api/finances/");
         var textResult = await response.Content.ReadAsStringAsync();
         var result = await response.Content.ReadFromJsonAsync<List<FinanceDTO>>();
 
         // Assert
         response.StatusCode.ShouldBe(System.Net.HttpStatusCode.OK);
         result.ShouldBeOfType<List<FinanceDTO>>();
+    }
+
+    [Fact]
+    public async Task Put_Should_Update_Finance_And_Return_NoContent()
+    {
+        // Arrange
+        var client = _factory.CreateClient();
+        var updateCommand = new UpdateFinanceCommand
+        {
+            Id = 1, 
+            Title = "Zaktualizowany Tytuł",
+            Amount = 500.00M,
+            FinanceType = FinanceType.Assets 
+        };
+
+        // Act
+        var response = await client.PutAsJsonAsync("/api/finances/", updateCommand);
+        var result = await response.Content.ReadAsStringAsync();
+
+        // Assert
+        response.StatusCode.ShouldBe(System.Net.HttpStatusCode.NoContent);
     }
 }
