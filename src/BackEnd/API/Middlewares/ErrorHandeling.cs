8644fd595c803f15818cc275af98c9e436972a7c
Yigrashill <tomek198734@wp.pl>
2024-04-04 18:48:00 +0200

    Add Simple Middlewares to Hendel The Global Errors.

---
+++ src/BackEnd/API/Middlewares/ErrorHandeling.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.Middlewares
+{
+    public class ErrorHandeling : IMiddleware
+    {
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+			try
+			{
+				await next.Invoke(context);
+			}
+			catch (Exception ex)
+			{
+				context.Response.StatusCode = 500;
+				await context.Response.WriteAsync($"Global Error: {ex.Message}");
+			}
+        }
+    }
+}
