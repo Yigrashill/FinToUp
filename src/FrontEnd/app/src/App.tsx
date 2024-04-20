2e7a121a1405333aa60c368ba146328b12e3691f
Yigrashill <tomek198734@wp.pl>
2024-04-20 12:13:57 +0200

    Add to aqpp ts

---
+++ src/FrontEnd/app/src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>Super</h2>
+
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
+ 
