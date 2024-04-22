92e7a9a5cc864144f3621606a39869b3a5fb993d
Yigrashill <tomek198734@wp.pl>
2024-04-22 22:29:31 +0200

    Some base changes in app

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -4,22 +4,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h2>Lern somting totaly new in React </h2>
+    </>
   );
 }
 
