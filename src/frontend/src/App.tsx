c48d528d763d6ef1798f844df84ded30bd8b0f90
Yigrashill <tomek198734@wp.pl>
2024-04-24 17:53:52 +0200

    Nauka jsx i tsx.

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,11 +1,36 @@
 const  App = () => {
 
-  
+  const name : string = 'John Doe';
+  const x : number = 10;
+  const y : number = 10;
+  const names = ['Brad', 'Mary', 'Joe', name];
+  const logIn : boolean = true;
+  const styles = {
+    color : 'red',
+    fontSize : '55px'
+  };
+
+
   return (
     <>
-      <div className="text-3xl">
-          Hello world!
+      <div className="text-5xl">
+        <p>Sum of {x} + {y} = {x + y} </p>
       </div>
+      <div>
+      <div style={styles}>
+        <p> Hello: {name}</p>
+      </div>
+        <p>{logIn ? 'true' : 'false' } </p>
+        {logIn &&  <p> Hello: {name}</p> } 
+      </div>
+      <div>
+        <ul>
+          {names.map((nam:string, index:number) =>
+            <li key={index}> {nam}</li>
+          )}
+        </ul>
+      </div>
+
     </>
   );
 }
