8c0384ed472fb546596d13f662ef3f58b0137127
Yigrashill <tomek198734@wp.pl>
2024-04-25 17:15:09 +0200

    add new component Hero

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from './Components/Navbar'
+import Hero from './Components/Hero'
 
 
 const App = () => {
   return (
     <>
       <Navbar />
+      <Hero />
 
     </>
   )
