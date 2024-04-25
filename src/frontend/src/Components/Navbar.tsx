9f6d006b0d69e592d2a235da7245efebbc9237cc
Yigrashill <tomek198734@wp.pl>
2024-04-25 17:14:23 +0200

    Fix in Navbar

--- src/frontend/src/Components/Navbar.tsx
+++ src/frontend/src/Components/Navbar.tsx
@@ -13,9 +13,20 @@ const Navbar = () => {
                             React Jobs
                         </span>
 
-                        <div className='md:ml-auto'>
-                            <div className='flex space=x-2'>
-                                Home
+                        <div className='md:ml-auto text-white '>
+                            <div className="flex space-x-2">
+                                <a href="/index.html"
+                                    className="text-white bg-black hover:bg-gray-800 hover:text-white rounded-md px-3 py-2">
+                                    Home
+                                </a>
+                                <a href='/jobs.html' 
+                                    className='text-white hover:bg-gray-800 hover:text-white rounded-md px-3 py-2'>
+                                    Jobs    
+                                </a>
+                                <a href='/add-jobs.html' 
+                                    className='text-white hover:bg-gray-800 hover:text-white rounded-md px-3 py-2'>
+                                    Add Job    
+                                </a>
                             </div>
                         </div>
                     </div>
