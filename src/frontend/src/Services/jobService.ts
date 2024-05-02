9ad961bcf1ddb6652e26fa0909382c6c87016d98
Yigrashill <tomek198734@wp.pl>
2024-05-02 10:19:23 +0200

    Difrent use of iHome to send data

--- src/frontend/src/Services/jobService.ts
+++ src/frontend/src/Services/jobService.ts
@@ -5,9 +5,9 @@ import { Job } from '../Types/Job';
 class JobService {
     private baseUrl: string = 'http://localhost:8000/jobs';
 
-    public async getJobs(): Promise<Job[]> {
+    public async getJobs(num :number): Promise<Job[]> {
         try {
-            const response = await axios.get<Job[]>(this.baseUrl);
+            const response = await axios.get<Job[]>(this.baseUrl + `?_limit=${num}`);
             return response.data;
         } catch (error) {
             throw new Error('error fetching jobs');
