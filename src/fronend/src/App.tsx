d3033aac691f718f75ca1f14323a8988382b1dfe
Yigrashill <tomek198734@wp.pl>
2024-05-14 22:06:04 +0200

    Install Tailwind css

--- src/fronend/src/App.tsx
+++ src/fronend/src/App.tsx
@@ -5,20 +5,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container bg-black text-white">
+      <h1 className=''>New Project</h1>
+
+      </div>
     </div>
   );
 }
