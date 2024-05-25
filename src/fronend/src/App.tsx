963b85852642f7e445314fb09a724a9371f6c573
Yigrashill <tomek198734@wp.pl>
2024-05-25 22:18:16 +0200

    Add Navbar Component

--- src/fronend/src/App.tsx
+++ src/fronend/src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import FinanceList from './Component/Finance/FinanceList';
+import Navbar from './Component/Navbar';
 
 function App() {
   return (
     <div className="App">
       <div className="w-full px-6 md:px-6 bg-sky-400 dark:bg-slate-900">
+        <Navbar />
       <FinanceList />
       </div>
     </div>
