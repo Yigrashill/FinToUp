47ec4260ef64483c263508192eb410e43dac52d3
Yigrashill <tomek198734@wp.pl>
2024-05-30 19:55:05 +0200

    Simple structure of Navbar Button

--- src/fronend/src/Component/Navbar.tsx
+++ src/fronend/src/Component/Navbar.tsx
@@ -4,29 +4,23 @@ import logo from '../images/logo.png';
 
 const Navbar = () => {
 
-  type LinkClassProps = {
-      isActive?: boolean; 
-    };
-    
-    const linkClass = ({ isActive = false }: LinkClassProps): string =>
-      isActive
-        ? 'bg-black text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2'
-        : 'text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2';
-
   return (
-      <nav className='bg-back'>
+      <nav className ='bg-stone-950'>
           <div className='mx-auto max-w-7xl px-2 sm:px-6 lg:px-8'>
               <div className='flex h-20 items-center justify-between'>
                   <div className='flex flex-1 items-center justity-center md:items-strecht md:justify-start'>
-                      <Link to='/' className='flex flex-shrink=0 items-center mr-4 ' >
+                      <Link to='/home' className='flex flex-shrink=0 items-center mr-4 ' >
                       <img className='h-10 w-auto' src={logo} alt='React Jobs' />
                       <span className='hidden md:block text-white text-2xl font-bold ml-2'>
-                          React Jobs
+                          Fin To Up
                       </span>
                       </Link>
-                      <div className='md:ml-auto text-white '>
-                          <div className="flex space-x-2">
-                              <NavLink to='' className= {linkClass}>Home</NavLink>
+                      <div className='md:ml-auto text-zinc-100  justify-center font-sans font-bold'>
+                          <div className="flex-1 bg-green-950 hover:bg-green-900 p-2">
+                              <NavLink to='/Dashbord'>Dashbord</NavLink>
+                          </div>
+                          <div className="flex-2 bg-green-950 hover:bg-green-900 p-2">
+                              <NavLink to='/Dashbord'>Dashbord</NavLink>
                           </div>
                       </div>
                   </div>
--- src/fronend/src/layouts/MainLayout.tsx
+++ src/fronend/src/layouts/MainLayout.tsx
@@ -6,13 +6,10 @@ import { Outlet } from 'react-router-dom'
 
 const MainLayout = () => {
   return (
-    <div className="w-full  items-center px-6 md:px-6 bg-black dark:bg-slate-900">
+    <div className="w-full px-3 md:px-6 bg-dark-800 dark:bg-slate-900">
       <Navbar />
-    
       <Hero />
       <Outlet />
-
-
     </div>
   )
 }
